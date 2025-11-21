98179145d93bd05e64d63fc77ee2af8c592efa89
KmiloVm <kmilo7v@gmail.com>
2025-11-20 21:56:25 -0500

    Add dotenv and configure a connection to MongoDB

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
 import express from "express";
+import mongoose from "mongoose";
+import apiRouter from "./routes/index.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -7,6 +12,19 @@ app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
+app.use(express.json());
+
+app.use("/api", apiRouter);
+
 app.listen(PORT, () => {
+  mongoose
+    .connect(process.env.MONGODB_URI)
+    .then(() => {
+      console.log("Connected to MongoDB");
+    })
+    .catch((error) => {
+      console.error("Error connecting to MongoDB:", error);
+    });
+
   console.log(`Server is running on http://localhost:${PORT}`);
 });
